47fb77dcbc36c9894baaff629bfeb700f0005e51
SeventhHokage <65212332+SeventhHokage@users.noreply.github.com>
2020-05-12 12:15:33 +0530

    app

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const orange = Color(0xffFF7433);
+const grey = Color(0xff767A80);
+
+String output = '0';
+double num1 = 0.0;
+double num2 = 0.0;
+String operand = '';
+String temp = '0';
+int flag = 0;
+String total = '';
+Color numbercolor;
+Color iconcolor;
+bool type = true;
+bool decimaladded=false;
